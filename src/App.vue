266a3e242746479ef46f8e302a5ad87519f411b4
王涛 <wangtao@jiagouyun.com>
2017-06-14 09:50:12 +0800

    router-link

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@
     <h3>首页导航栏</h3>
     <div>
       <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="1"><a href="/">处理中心</a></el-menu-item>
+        <el-menu-item index="1"><router-link to="/home">主页</router-link></el-menu-item>
         <el-submenu index="2">
           <template slot="title">我的工作台</template>
           <el-menu-item index="2-1">选项1</el-menu-item>
           <el-menu-item index="2-2">选项2</el-menu-item>
           <el-menu-item index="2-3">选项3</el-menu-item>
         </el-submenu>
-        <el-menu-item index="3"><a href="" >订单管理</a></el-menu-item>
-        <el-menu-item index="4"><a href="" >第三个</a></el-menu-item>
-        <el-menu-item index="5"><a href="" >第四个</a></el-menu-item>
+        <el-menu-item index="3"><router-link to="/page01">第二页</router-link></el-menu-item>
+        <el-menu-item index="4"><router-link to="/page02">第三页</router-link></el-menu-item>
+        <el-menu-item index="5"><router-link to="/page03">第四页</router-link></el-menu-item>
       </el-menu>
     </div>
     <ul class="home-nav">
